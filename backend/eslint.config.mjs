65f2d17fe663a72a41b66e72d8ffadd3aec63c19
nikunjjadav123 <dirensj@gmail.com>
2025-12-29 21:43:12 +0530

    added error handling logic through middleware

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -14,7 +14,9 @@ export default [
       }
     },
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^_"
+      }],
       "no-console": "off"
     }
   }

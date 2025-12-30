9e57a91162f238cfa3faf65df8586a9ee0c4b0c5
nikunjjadav123 <dirensj@gmail.com>
2025-12-30 17:23:00 +0530

    Added Role based medicine access (if role pharmasist then access all medicines APIs) and also created a single admin

--- backend/src/middleware/restrictTo.js
+++ backend/src/middleware/restrictTo.js
@@ -3,7 +3,6 @@ const prisma = require("../prisma");
 
 const restrictTo = (...roles) => {
   return async(req, res, next) => {
-    const user = await prisma.user.findUnique({ where: { id: req.user.id }});
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
         status: "fail",

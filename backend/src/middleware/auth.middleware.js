ad8732208b3af3e710757235a65f750a995367d5
nikunjjadav123 <dirensj@gmail.com>
2026-01-03 17:53:34 +0530

    add comments in auth middleware file

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -11,13 +11,17 @@ const auth = async (req, res, next) => {
         message: 'Token required',
       });
     }
+    /* Check if User or Admin Logout or not Start */
     const blocked = await redis.get(`bl_token:${token}`);
 
     if (blocked) {
       return res.status(401).json({ message: 'Logged out' });
     }
+    /* Check if User or Admin Logout or not End */
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    
+
+    /* Check if forced logout from all devices Start */
     const user = await prisma.user.findUnique({
       where: { id: decoded.id },
     });
@@ -25,8 +29,11 @@ const auth = async (req, res, next) => {
     if (decoded.tokenVersion !== user.tokenVersion) {
       return res.status(401).json({ message: "Session expired so please login again" });
     }
-    
+
+    /* Check if forced logout from all devices End */
+
     req.user = decoded;
+
     next();
   } catch (err) {
     return res.status(401).json({
